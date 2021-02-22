81479074edadadd2271039e8334ef47444fc824c
LeeMintae <lightwire@soulsoft.co.kr>
2021-02-22 15:25:12 +0900

    page, function updated

---
+++ app/src/main/java/kr/co/soulsoft/aitest200911/ExercisePlanActivity.java
@@ -0,0 +1,14 @@
+package kr.co.soulsoft.aitest200911;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExercisePlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_plan);
+    }
+}
